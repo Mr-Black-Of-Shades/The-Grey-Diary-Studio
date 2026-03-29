f2e75fe9e46d5eca9eb73f7f408919d3633fc1d9
By Shiladitya Mallick <info.shiladityamallick@gmail.com>
2026-03-30 02:36:05 +0530

    Refactor episode creation form with styles

--- app/studio/episodes/create/page.js
+++ app/studio/episodes/create/page.js
@@ -5,11 +5,30 @@ export default function CreateEpisode() {
     <div>
       <h1>Create Episode</h1>
 
-      <input placeholder="Title" /><br /><br />
-      <input placeholder="Type (main/side/fan)" /><br /><br />
-      <input placeholder="Price" /><br /><br />
+      <div style={{ marginTop: 20 }}>
+        <input style={input} placeholder="Title" />
+        <input style={input} placeholder="Type (main/side/fan)" />
+        <input style={input} placeholder="Price ₹" />
 
-      <button>Create</button>
+        <button style={primaryBtn}>Create Episode</button>
+      </div>
     </div>
   );
 }
+
+const input = {
+  display: "block",
+  marginBottom: 10,
+  padding: 10,
+  width: 300,
+  borderRadius: 6,
+  border: "1px solid #ccc",
+};
+
+const primaryBtn = {
+  padding: "10px 20px",
+  borderRadius: 8,
+  border: "none",
+  background: "black",
+  color: "white",
+};
